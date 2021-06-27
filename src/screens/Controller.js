a7ef9523b825b33cf7ab993c9f2ec2682814f37a
John Doe <john@sample.com>
2021-06-27 23:01:15 +0530

    Updated controller file

--- src/screens/Controller.js
+++ src/screens/Controller.js
@@ -1,20 +1,18 @@
 import React, { Component } from 'react';
 import Home from '../screens/home/Home';
 import Login from '../screens/login/Login';
+import Profile from "../screens/profile/Profile";
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 class Controller extends Component {
 
-  constructor() {
-    super();
-    this.baseUrl = "http://localhost:8080/";
-  }
   render() {
     return (
       <Router>
         <div className="main-container">
           <Route exact path='/' render={(props) => <Login {...props} baseUrl={this.baseUrl} />} />
           <Route path='/home' render={(props) => <Home {...props} />} />
+          <Route path="/profile" render={(props) => <Profile {...props} />} />
         </div>
       </Router>
     )
