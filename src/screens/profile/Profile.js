770a237508365c452f84457f13f9309410de16d9
John Doe <john@sample.com>
2021-06-28 09:20:22 +0530

    Code for header

--- src/screens/profile/Profile.js
+++ src/screens/profile/Profile.js
@@ -1,7 +1,25 @@
+import React, { Component } from 'react';
+import Header from '../../common/header/Header';
+import './Profile.css';
+import Avatar from '@material-ui/core/Avatar';
+
+import instaLogo from "../../assets/insta.png";
 class Profile extends Component {
     render (){
         return(
             <div>
+                 <Header
+          title="Image Viewer"
+          history={this.props.history}
+          showProfilePage="profile"
+        />
+        <div className="top-container">
+          <Avatar className="profile-picture-avatar"
+            alt="User Image"
+            style={{ cursor: 'pointer' }}
+            src={instaLogo}
+          />
+          </div>
 
             </div>
         )
